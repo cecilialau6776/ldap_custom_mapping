6475a5a9f561275a93c5a9c2555687b48706a621
Cecilia Lau <cecilialau6776@gmail.com>
2023-07-12 14:16:58 -0400

    search_a_user uses search_by to search by attribute, fix parse_ldap_entry

--- coldfront_plugin_ldap_custom_mapping/utils.py
+++ coldfront_plugin_ldap_custom_mapping/utils.py
@@ -14,23 +14,23 @@ class LDAPCustomMapping(UserSearch):
 
     def __init__(self, user_search_string, search_by):
         super().__init__(user_search_string, search_by)
-        self.LDAP_SERVER_URI = import_from_settings('LDAP_USER_SEARCH_SERVER_URI')
-        self.LDAP_USER_SEARCH_BASE = import_from_settings('LDAP_USER_SEARCH_BASE')
-        self.LDAP_BIND_DN = import_from_settings('LDAP_USER_SEARCH_BIND_DN', None)
-        self.LDAP_BIND_PASSWORD = import_from_settings('LDAP_USER_SEARCH_BIND_PASSWORD', None)
-        self.LDAP_CONNECT_TIMEOUT = import_from_settings('LDAP_USER_SEARCH_CONNECT_TIMEOUT', 2.5)
-        self.LDAP_USE_SSL = import_from_settings('LDAP_USER_SEARCH_USE_SSL', True)
+        self.LDAP_SERVER_URI = import_from_settings("LDAP_USER_SEARCH_SERVER_URI")
+        self.LDAP_USER_SEARCH_BASE = import_from_settings("LDAP_USER_SEARCH_BASE")
+        self.LDAP_BIND_DN = import_from_settings("LDAP_USER_SEARCH_BIND_DN", None)
+        self.LDAP_BIND_PASSWORD = import_from_settings("LDAP_USER_SEARCH_BIND_PASSWORD", None)
+        self.LDAP_CONNECT_TIMEOUT = import_from_settings("LDAP_USER_SEARCH_CONNECT_TIMEOUT", 2.5)
+        self.LDAP_USE_SSL = import_from_settings("LDAP_USER_SEARCH_USE_SSL", True)
         self.LDAP_USE_TLS = import_from_settings("LDAP_USER_SEARCH_USE_TLS", False)
-        self.LDAP_PRIV_KEY_FILE = import_from_settings('LDAP_USER_SEARCH_PRIV_KEY_FILE', None)
-        self.LDAP_CERT_FILE = import_from_settings('LDAP_USER_SEARCH_CERT_FILE', None)
-        self.LDAP_CACERT_FILE = import_from_settings('LDAP_USER_SEARCH_CACERT_FILE', None)
-        self.ATTRIBUTE_MAP = import_from_settings('LDAP_USER_SEARCH_ATTRIBUTE_MAP', {
+        self.LDAP_PRIV_KEY_FILE = import_from_settings("LDAP_USER_SEARCH_PRIV_KEY_FILE", None)
+        self.LDAP_CERT_FILE = import_from_settings("LDAP_USER_SEARCH_CERT_FILE", None)
+        self.LDAP_CACERT_FILE = import_from_settings("LDAP_USER_SEARCH_CACERT_FILE", None)
+        self.ATTRIBUTE_MAP = import_from_settings("LDAP_USER_SEARCH_ATTRIBUTE_MAP", {
                                                       "username": "uid",
                                                       "last_name": "sn",
                                                       "first_name": "givenName",
                                                       "email": "mail",
                                                   })
-        self.MAPPING_CALLBACK = import_from_settings('LDAP_USER_SEARCH_MAPPING_CALLBACK', self.parse_ldap_entry)
+        self.MAPPING_CALLBACK = import_from_settings("LDAP_USER_SEARCH_MAPPING_CALLBACK", self.parse_ldap_entry)
 
         tls = None
         if self.LDAP_USE_TLS:
@@ -43,11 +43,11 @@ class LDAPCustomMapping(UserSearch):
         self.server = Server(self.LDAP_SERVER_URI, use_ssl=self.LDAP_USE_SSL, connect_timeout=self.LDAP_CONNECT_TIMEOUT, tls=tls)
         self.conn = Connection(self.server, self.LDAP_BIND_DN, self.LDAP_BIND_PASSWORD, auto_bind=True)
 
+    @staticmethod
     def parse_ldap_entry(attribute_map, entry_dict):
         user_dict = {}
-        for user_attr, ldap_attr in attribute_map:
+        for user_attr, ldap_attr in attribute_map.items():
             user_dict[user_attr] = entry_dict.get(ldap_attr)[0] if entry_dict.get(ldap_attr) else ''
-
         return user_dict
 
     def search_a_user(self, user_search_string=None, search_by='all_fields'):
@@ -60,8 +60,8 @@ class LDAPCustomMapping(UserSearch):
             filter = ldap.filter.filter_format(
                 f"(|({ldap_attrs[0]}=*%s*)({ldap_attrs[1]}=*%s*)({ldap_attrs[2]}=*%s*)({ldap_attrs[3]}=*%s*))",
                 [user_search_string] * 4)
-        elif user_search_string and search_by == 'username_only':
-            filter = ldap.filter.filter_format(f"({self.ATTRIBUTE_MAP['username']}=%s)",
+        elif user_search_string:
+            filter = ldap.filter.filter_format(f"({self.ATTRIBUTE_MAP[search_by]}=%s)",
                                                [user_search_string])
             size_limit = 1
         else:
